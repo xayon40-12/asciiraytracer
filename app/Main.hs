b42720a1e2a868484f53791829dd7371b15196c5
xayon40-12 <touroux.nathan@gmail.com>
2021-04-30 19:23:39 +0200

    use deepseq to compute time better, not perfect

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,11 @@ module Main where
 
 import Camera
 import Control.Concurrent
+import Control.DeepSeq
 import Control.Monad
-import qualified Data.ByteString.Char8 as B
 import Data.Function ((&))
 import Data.Maybe
+import qualified Data.Text.IO as T
 import Render
 import qualified Render.CNode
 import Render.Color
@@ -39,14 +40,14 @@ main = do
   let int = location [Scale 2, Translate (2 *. ey)] $ s <.> s2 <.> s3
   let uni = location [Scale 0.5, Rotate ey (3 * pi / 4), Translate ((-1) *. ey .+. 1 *. ez)] $ s >.< s2 >.< s3
   let sub = location [Scale 0.5, Rotate ey (- pi / 4), Translate ((-2) *. ez)] $ s <.< s2 <.< s3
-  let o = [int, uni, sub, st]
+  let o = [int, uni, sub, st] ++ map (\i -> sphere (i *. ex) 0.3 yellow) [0 .. 100]
   let f = 600
   forM_ [0 ..] $ \i -> do
     start <- getCPUTime
-    let !can = canvas (lookat (rot ey (10 *. ex) (i / f * 2 * pi)) e0) (100, 100) (View 1 1) o
+    let !can = let tmp = canvas (lookat (rot ey (10 *. ex) (i / f * 2 * pi)) e0) (100, 100) (View 1 1) o in tmp `deepseq` tmp
     ecan <- getCPUTime
-    let !disp = dispc can
+    let !disp = let tmp = dispc can in tmp `deepseq` tmp
     edis <- getCPUTime
-    B.putStrLn disp
+    T.putStrLn disp
     end <- getCPUTime
     printf "canvas: %0.3f s\ndispc: %0.3f s\nputStrLn: %0.3f s\n" (diff start ecan) (diff ecan edis) (diff edis end)
